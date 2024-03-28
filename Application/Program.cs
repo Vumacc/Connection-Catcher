7485f07a1bc63a6f0d3004c71ff182d7d7446d57
Vumacc <vumacc7@gmail.com>
2024-03-28 17:06:24 +0500

    Initial Commit

---
+++ Application/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Dynamic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConnectionCatcher
+{
+    class ConnectionCatcher
+    {
+
+        static void Main(string[] args)
+        {
+            // Setting up the TcpListener to catch incoming traffic
+            TcpListener Listener = new TcpListener(IPAddress.Any, 80);
+            Listener.Start();
+
+            Console.WriteLine("Listening for incoming connections...");
+
+            while (true)
+            {
+                // Start accepting incoming connections
+                TcpClient client = Listener.AcceptTcpClient();
+
+                // Handle connections in seperate threads
+                System.Threading.Thread thread = new System.Threading.Thread(() => HandleConnections(client));
+                thread.Start();
+            }
+        }
+
+        static void HandleConnections(TcpClient client)
+        {
+            // Get the clients IP and port information
+            IPEndPoint clientEndPoint = (IPEndPoint)client.Client.RemoteEndPoint;
+            string clientIP = clientEndPoint.Address.ToString();
+            int clientPort = clientEndPoint.Port;
+            Console.WriteLine($"New connection from: {clientIP}:{clientPort}");
+
+            // Printing out the traffic information
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string dataReceived = System.Text.Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            Console.WriteLine($"Data received: {dataReceived}");
+
+            // Close the connection
+            client.Close();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Simple Connection Catcher
+This is simple CLI application made using C# that catches the connections made onto your machine, and also shows some information about said connection.
